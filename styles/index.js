9a38c592f4ee262e71178ddc30fa616eb4f77dfa
Nasha <natalif158@gmail.com>
2023-05-28 20:32:32 -0300

    cambios en formato

--- styles/index.js
+++ styles/index.js
@@ -1,12 +1,2 @@
-function firtsFunction(number1, number2) {
-    let suma = number1 + number2;
-    return suma}; 
-    
-function functionResta(number1, number2) {
-    let resta = number1 - number2;
-    return resta};
-  
-function functionAlert(mensaje){
-    return alert(mensaje);
-}
+
 
